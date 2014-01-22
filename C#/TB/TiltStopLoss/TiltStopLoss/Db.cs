540b37ba88a4d72c0d714df158949bc747e24e07
ricain59 <ricain59@gmail.com>
2014-01-22 23:04:31 +0000

    - DT: add zoom, vm only for this - TB: début de création de mon soft de stop loss

---
+++ C#/TB/TiltStopLoss/TiltStopLoss/Db.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+using System.Windows.Forms;
+
+namespace TiltStopLoss
+{
+    class Db
+    {
+        private String user;
+        private String server;
+        private String port;
+        private String password;
+        private String database;
+        private String connstring;
+        private NpgsqlConnection conn;
+
+        public void getData(String user, String server, String port, String password, String Db)
+        {
+            this.user = user;
+            this.server = server;
+            this.port = port;
+            this.password = password;
+            this.database = Db;
+            this.connstring = String.Format("Server={0};Port={1};" +
+                            "User Id={2};Password={3};Database={4};",
+                            server, port, user, password, database);
+        }
+        
+        public String testconnectDb()
+        {
+            try
+            {
+                //PostgeSQL-style connection string
+                conn = new NpgsqlConnection(connstring);
+                
+                conn.Open();
+                conn.Close();
+                return "";
+            }
+            catch (Exception msg)
+            {
+                return msg.ToString();
+            }
+        }
+
+        public String connectDb()
+        {
+            try
+            {
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                return "";
+            }
+            catch (Exception msg)
+            {
+                return msg.ToString();
+            }
+        }       
+    }
+}
---
+++ C#/TB/TiltStopLoss/TiltStopLoss/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TiltStopLoss
+{
+    public partial class Main : Form
+    {
+        Db db;
+        
+        public Main()
+        {
+            InitializeComponent();
+            db = new Db();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTestDb_Click(object sender, EventArgs e)
+        {
+            db.getData(textBoxUser.Text, textBoxServer.Text, textBoxPort.Text, textBoxPass.Text, textBoxDb.Text);
+            String testconnect = db.testconnectDb();
+            if (testconnect.Equals(""))
+            {
+                MessageBox.Show("Test connection OK");
+            }
+            else
+            {
+                MessageBox.Show(testconnect);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HandPs ps = new HandPs();
+            //ligo me a DB para ir buscar o ultimo id inserido
+            String con = db.connectDb();
+            if (!con.Equals(""))
+            {
+                MessageBox.Show(con.ToString());
+            }
+
+            //DEPOIS de recuperar o ultimo id pego na mão importado e tenho de verificar se 
+            //o meu nome de jogador esta la
+
+            //se sim tratou a mão
+
+            //aqui vou tratar a mão e devolve me os BB perdido ou ganhos
+
+            
+        }
+
+
+    }
+}
